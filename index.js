a7e1a97ed26bb53c3f1a1ea33d1c3ff22d117b71
Gabriel7721 <176547965+tribta@users.noreply.github.com>
2025-07-03 21:36:15 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ function HtmlElement() {
     console.log("Click the input element!!!");
   };
 }
+const items = [];
+
+function HtmlSelectElement(items) {
+  this.items = items || [];
+
+  // addItem(item) => push && append
+  this.addItem = function (item) {
+    this.items.push(item);
+  };
+  // removeItem(item) => slice || splice || filter
+  this.removeItem = function (item) {
+    this.items = this.items.filter((i) => i !== item);
+  };
+}
+
+// kế thừa:
+HtmlSelectElement.prototype = new HtmlElement();
+HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+
+// check yêu cầu số 2:
+const array = new HtmlSelectElement();
+
+array.items = [1, 2, 3, 4, 5];
+array.removeItem(1);
+array.addItem(6);
 
 // 2. focus()
 // syntax: HtmlElement.prototype
@@ -16,3 +41,9 @@ HtmlElement.prototype.focus = function () {
 const input = new HtmlElement();
 input.click();
 input.focus();
+
+const render = function (a) {
+  return `<input>\n
+           <option>${a}\n
+        <input>\n`;
+};
